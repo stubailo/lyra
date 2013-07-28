cb451ba1a1f6c7233d055059204fb9b30e78ece0
Kevin Wu <kevinw@palantir.com>
2013-07-27 18:32:50 -0700

    Added comments for ContextNode and added an initialize method.

--- src/node.ts
+++ src/node.ts
@@ -6,28 +6,54 @@ class ContextNode extends Backbone.Model {
   private _context: Context;
   private _name: string;
 
+  // This event is triggered when a context node has finished rendering.
   public static EVENT_READY: string = "EVENT_READY";
 
+  /* Utility method that returns an array of ContextNodes of a certain type.
+   *
+   * @param specList A list of specifications that will be passed to each new node
+   * @param context The universal context
+   * @param classType A class (should be extending ContextNode) that will be created
+   * @return An array of ContextNodes of type classtype
+   */
   public static parseAll(specList: any[], context: Context, classType: any) : any[] {
     return _.map(specList, function(spec) {
       return classType.parse(spec, context);
     });
   }
 
+  /* Creates a ContextNode, setting up the name, context, and properties from the specification
+   * passed to it.
+   *
+   * This method should not be overriden. Instead, override the intialize method to perform additional
+   * operations before the contextNode is rendered.
+   */
   constructor (spec: any, context: Context, className: string) {
     super();
+    // Setup instance variables
     this._name = spec["name"];
     this._context = context;
+    // Save this ContextNode in the context
     this._context.set(className + ":" + this.name, this);
+    // Parse the properties of this node from the specification
     this.parseProperties(spec);
+    // Additional initialization
+    this.initialize()
 
+    // Event to be removed
     this.refresh();
     this.on("change", () => {
       this.refresh();
     });
   }
 
-  // look at all of the spec properties
+  /* Given a hash of properties, attaches the properties to the contextNode.
+   *
+   * This method parses three type of property values.
+   * 1) Static numerical values
+   * 2) Objects
+   * 3) Property References (e.g. Set the property width of the contextNode to be the width of another contextNode)
+   */
   public parseProperties(properties: any): void {
     for(var key in properties) {
       var value = properties[key];
@@ -48,16 +74,6 @@ class ContextNode extends Backbone.Model {
     }
   }
 
-  private static isPropertyReference(obj: string) {
-    var propertyRegex = /^[A-Za-z_\-0-9]+:[A-Za-z_\-0-9]+\.[A-Za-z_\-0-9]+$/;
-    return ((typeof(obj) === "string") && propertyRegex.test(obj));
-  }
-
-  private static isObjectReference(obj: string) {
-    var objectRegex = /^[A-Za-z_\-0-9]+:[A-Za-z_\-0-9]+$/;
-    return ((typeof(obj) === "string") && objectRegex.test(obj));
-  }
-
   public get name(): string {
     return this._name;
   }
@@ -68,6 +84,24 @@ class ContextNode extends Backbone.Model {
 
   public recalculate(callback) {
     callback();
+  }
+
+  /* Private method to check if a property string is a property reference.
+   *
+   * This method checks if the string is of the form: <className>:<name>.<property>
+   */
+  private static isPropertyReference(obj: string) {
+    var propertyRegex = /^[A-Za-z_\-0-9]+:[A-Za-z_\-0-9]+\.[A-Za-z_\-0-9]+$/;
+    return propertyRegex.test(obj);
+  }
+
+  /* Private method to check if a property string is an object reference.
+   *
+   * This method checks if the string is of the form: <className>:<name>
+   */
+  private static isObjectReference(obj: string) {
+    var objectRegex = /^[A-Za-z_\-0-9]+:[A-Za-z_\-0-9]+$/;
+    return objectRegex.test(obj);
   }
 
   private refresh() {
