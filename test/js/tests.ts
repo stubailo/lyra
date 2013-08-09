22d524350dff8297cf28d047cda98d6488169d68
Sashko Stubailo <sstubailo@palantir.com>
2013-08-08 18:01:33 -0700

    Added some tests

--- test/js/tests.ts
+++ test/js/tests.ts
@@ -25,25 +25,79 @@ module Lyra {
             });
         });
 
-        describe("Property Events", function() {
-            it("Correctly fires events", function() {
-                var context: Context = new Context();
+        describe("Context Node", function() {
+            describe("Housekeeping", function() {
+                it("Adds itself to the Context properly", function() {
+                    var NAME_PROPERTY = "fred";
+                    var NODE_CLASSNAME = "node";
 
-                var node1 = new ContextNode({
-                    name: "node1",
-                    prop: 3
-                }, context, "Node");
+                    var context: Context = new Context();
+                    var node = new ContextNode({
+                        name: NAME_PROPERTY
+                    }, context, NODE_CLASSNAME);
 
-                var node2 = new ContextNode({
-                    name: "node2",
-                    prop: "Node:node1.prop"
-                }, context, "Node");
+                    assert.equal(node, context.getNode(NODE_CLASSNAME, NAME_PROPERTY));
+                });
+            });
 
-                assert.equal(node1.get("prop"), node2.get("prop"));
+            describe("Parsing properties", function() {
+                it("Correctly parses plain spec properties", function() {
+                    var context: Context = new Context();
 
-                node1.set("prop", 4);
+                    var NODE_CLASSNAME = "node";
+                    var NAME_PROPERTY = "node1";
+                    var NUMBER_PROPERTY = 3;
+                    var STRING_PROPERTY = "string";
+                    var DATE_PROPERTY = new Date();
 
-                assert.equal(node1.get("prop"), node2.get("prop"));
+                    var node1 = new ContextNode({
+                        name: NAME_PROPERTY,
+                        number: NUMBER_PROPERTY,
+                        string: STRING_PROPERTY,
+                        date: DATE_PROPERTY
+                    }, context, NODE_CLASSNAME);
+
+                    assert.equal(node1.name, NAME_PROPERTY);
+                    assert.equal(node1.get("number"), NUMBER_PROPERTY);
+                    assert.equal(node1.get("string"), STRING_PROPERTY);
+                    assert.equal(node1.get("date"), DATE_PROPERTY);
+                });
+
+                it("Correctly parses object references", function() {
+                    var context: Context = new Context();
+
+                    var node1 = new ContextNode({
+                        name: "node1",
+                        prop: 3
+                    }, context, "Node");
+
+                    var node2 = new ContextNode({
+                        name: "node2",
+                        otherNode: "Node:node1"
+                    }, context, "Node");
+
+                    assert.equal(node1, node2.get("otherNode"));
+                });
+
+                it("Correctly parses and updates property references", function() {
+                    var context: Context = new Context();
+
+                    var node1 = new ContextNode({
+                        name: "node1",
+                        prop: 3
+                    }, context, "Node");
+
+                    var node2 = new ContextNode({
+                        name: "node2",
+                        prop: "Node:node1.prop"
+                    }, context, "Node");
+
+                    assert.equal(node1.get("prop"), node2.get("prop"));
+
+                    node1.set("prop", 4);
+
+                    assert.equal(node1.get("prop"), node2.get("prop"));
+                });
             });
         });
     })();
